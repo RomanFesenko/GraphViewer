fd84fc6dcead973de3c4e456f1e068e9379b9a6d
Roman Fesenko <72913560+RomanFesenko@users.noreply.github.com>
2022-01-11 17:12:15 +0200

    Light source definitions

---
+++ light_source.cpp
@@ -0,0 +1,58 @@
+
+#include "light_source.h"
+
+CLightSource&CLightSource::SetPosition(const point_t&pos)
+{
+    m_position=pos;
+    return *this;
+}
+
+CLightSource&CLightSource::SetAmbient(const point_t&am)
+{
+    m_ambient=am;
+    return *this;
+}
+
+CLightSource&CLightSource::SetDiffuse(const point_t&diff)
+{
+    m_diffuse=diff;
+    return *this;
+}
+
+CLightSource&CLightSource::SetSpecular(const point_t&spec)
+{
+    m_specular=spec;
+    return *this;
+}
+
+CLightSource&CLightSource::SetAttenuation(const point_t&att)
+{
+    m_attenuation=att;
+    return *this;
+}
+
+CLightSource& CLightSource::SetPosition(float x,float y,float z)
+{
+    return SetPosition({x,y,z});
+}
+
+CLightSource& CLightSource::SetAmbient(float x,float y,float z)
+{
+    return SetAmbient({x,y,z});
+}
+
+CLightSource& CLightSource::SetDiffuse(float x,float y,float z)
+{
+    return SetDiffuse({x,y,z});
+}
+
+CLightSource& CLightSource::SetSpecular(float x,float y,float z)
+{
+    return SetSpecular({x,y,z});
+}
+
+CLightSource& CLightSource::SetAttenuation(float x,float y,float z)
+{
+    return SetAttenuation({x,y,z});
+}
+
---
+++ light_source.h
@@ -0,0 +1,41 @@
+
+#ifndef  _light_source_
+#define  _light_source_
+
+#include <Eigen/Core>
+
+
+class CLightSource
+{
+    using point_t=Eigen::Vector3f;
+    point_t m_position={0,0,5};
+    point_t m_ambient={0.1,0.1,0.1};
+    point_t m_diffuse={0.5,0.5,0.5};
+    point_t m_specular={1.0,1.0,1.0};
+    point_t m_attenuation={1.1,0.0,0.0};
+
+    public:
+    CLightSource() {}
+    CLightSource&SetPosition(const point_t&);
+    CLightSource&SetAmbient(const point_t&);
+    CLightSource&SetDiffuse(const point_t&);
+    CLightSource&SetSpecular(const point_t&);
+    CLightSource&SetAttenuation(const point_t&);
+
+    CLightSource&SetPosition(float,float,float);
+    CLightSource&SetAmbient(float,float,float);
+    CLightSource&SetDiffuse(float,float,float);
+    CLightSource&SetSpecular(float,float,float);
+    CLightSource&SetAttenuation(float,float,float);
+
+    const point_t&Position()const{return m_position;}
+    const point_t&Ambient()const{return m_ambient;}
+    const point_t&Diffuse()const{return m_diffuse;}
+    const point_t&Specular()const{return m_specular;}
+    const point_t&Attenuation()const{return m_attenuation;}
+
+};
+
+
+#endif
+
