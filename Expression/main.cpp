7cb56d97fb0c8b565883b6d827114393cca4ae35
Roman Fesenko <72913560+RomanFesenko@users.noreply.github.com>
2022-02-04 10:49:01 +0200

    Testing

---
+++ Expression/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <assert.h>
+#include "test/test_dependency_graph.h"
+#include "test/test_parsing.h"
+#include "test/test_function_pool.h"
+#include "test/benchmarks.h"
+
+
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    test_dependency_graph();
+    test_parsing();
+    test_function_pool();
+    test_benchmarks();
+    return 0;
+}
